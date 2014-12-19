4033b7b429e0029f4f2500d81a621975b6d4695e
Eike von Seggern <eikevons@yahoo.de>
2014-12-19 15:38:09 +0100

    fix for RTD

--- doc/conf.py
+++ doc/conf.py
@@ -25,9 +25,10 @@ if os.environ.get("READTHEDOCS"):
     class Mock(MagicMock):
         @classmethod
         def __getattr__(cls, name):
+
             return Mock()
 
-    MOCK_MODULES = ['numpy', 'scipy']
+    MOCK_MODULES = ["numpy", "scipy", "scipy.optimize", "matplotlib"]
     sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # If extensions (or modules to document with autodoc) are in another directory,
