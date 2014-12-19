71a7f09cb1a21d843b0d80336b504322af9a7b65
Eike von Seggern <eikevons@yahoo.de>
2014-12-19 15:35:50 +0100

    fix last fix

--- doc/conf.py
+++ doc/conf.py
@@ -21,9 +21,10 @@ if os.environ.get("READTHEDOCS"):
         from unittest.mock import MagicMock
     except ImportError:
         from mock import Mock as MagicMock
+
     class Mock(MagicMock):
         @classmethod
-        def __getattr(cls, name):
+        def __getattr__(cls, name):
             return Mock()
 
     MOCK_MODULES = ['numpy', 'scipy']
